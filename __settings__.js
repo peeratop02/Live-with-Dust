3e0e305d37ebdc194714e90bb9c6b6219364c697
Peerawit <peeratop02@gmail.com>
2021-05-24 12:49:16 +0700

    upload game verion 1.1.0b

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1140944.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 45450888, 45450889, 45450891, 47845248, 47846286, 47847462, 48064505, 48159729, 48170888, 48172062, 48245303, 48324784, 48325202, 48367611, 48367920, 48373676, 48382159 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
